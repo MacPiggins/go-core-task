3420ea7b075a2672bd992879e0babd8a80d71688
MacPiggins <macpiggins@yandex.ru>
2026-08-12 22:07:47 +0300

    task-4

---
+++ task-4/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func makeFilter(s []string) map[string]bool {
+	filter := make(map[string]bool)
+	for _, v := range s {
+		filter[v] = true
+	}
+	return filter
+}
+
+func filter(s1, s2 []string) []string {
+	filter := makeFilter(s2)
+	for i, v := range s1 {
+		if _, ok := filter[v]; ok {
+			s1 = append(s1[:i], s1[i+1:]...)
+		}
+	}
+	return s1
+}
+
+func main() {
+	slice1 := []string{"apple", "banana", "cherry", "date", "43", "lead", "gno1"}
+	slice2 := []string{"banana", "date", "fig"}
+	fmt.Println(filter(slice1, slice2))
+}
